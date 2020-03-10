0b6ec37c9e834c71ff696bf501f960caac5dbe0f
Hadiqaa <bc180402474@vu.edu.pk>
2020-03-10 09:28:41 +0500

    footer

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,13 +6,13 @@ import Layout from "../components/layout"
 import SEO from "../components/seo"
 import Hadiqa from "../components/hadiqa/hadiqa"
 import Banner from '../components/banner/banner'
-
+import Footer from '../components/footer/footer'
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <Banner />
     <Hadiqa />
-
+    <Footer />
   </Layout>
 )
 
